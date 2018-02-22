fe2427fd3dd225f51a7c532386736f69c54ff73f
MaxShepovalov <makson4622@gmail.com>
2018-02-21 19:52:22 -0800

    hw3. memory allocation update

--- hw3/graph_color2.cu
+++ hw3/graph_color2.cu
@@ -24,8 +24,8 @@ void ReadMMFile(const char filename[], bool** graph, int* V);
 
 //Kernel work with pairs of vertexes
 __global__ void KernelNeighbourColor(bool* graph, int* colors, bool* output, int V, int* job){
-    int job_index = floorf(threadIdx.x/V); //primary vertex selector from job list
-    int near  = threadIdx.x % V;           //neighbor vertex index         (col of graph)
+    int job_index = floorf((blockIdx.x * blockDim.x + threadIdx.x)/V); //primary vertex selector from job list
+    int near  = (blockIdx.x * blockDim.x + threadIdx.x) % V;           //neighbor vertex index         (col of graph)
     int index = job[job_index];            //primary vertex index;
 
     //stage 1. scan neighbour
@@ -43,7 +43,7 @@ __global__ void KernelNeighbourColor(bool* graph, int* colors, bool* output, int
 }
 
 __global__ void KernelSearchColor(int* colors, bool* nearcolors, int V, int* job){
-    int job_index = threadIdx.x; //job index
+    int job_index = blockIdx.x * blockDim.x + threadIdx.x; //job index
     int index = job[job_index];  //vertex index
     for (int clr = 1; clr < V; clr ++){
         if (!nearcolors[job_index * V + clr]){
@@ -54,9 +54,12 @@ __global__ void KernelSearchColor(int* colors, bool* nearcolors, int V, int* job
 }
 
 __global__ void KernelCheckColor(bool* graph, int* colors, int V, int* job, int* new_job){
-    int job_index = floorf(threadIdx.x/V); //primary vertex selector from job list
-    int near  = threadIdx.x % V;           //neighbor vertex index         (col of graph)
+    int job_index = floorf((blockIdx.x * blockDim.x + threadIdx.x)/V); //primary vertex selector from job list
+    int near  = (blockIdx.x * blockDim.x + threadIdx.x) % V;           //neighbor vertex index         (col of graph)
     int index = job[job_index];            //primary vertex index;
+    if (near == 0){
+        new_job[job_index] = -1;
+    }
     if (graph[index * V + near]){
         if (colors[near] == colors[index] and near > index){
             new_job[job_index] = index;
@@ -80,184 +83,201 @@ void GraphColoringGPU(const char filename[], int** color){
         return;
     }
 
+    //read free memory
+    size_t free, total;
+    cudaMemGetInfo(&free,&total); 
+    printf("\nGPU: %d KB free of total %d KB\n",free/1024,total/1024);
+
     //allocate list of colors per vector
     cudaError malloc_err = cudaMallocManaged(color, V * sizeof(int));
         if (malloc_err != cudaSuccess){
             std::cout << "COLOR_MALLOC cuda malloc ERROR happened: " << cudaGetErrorName(malloc_err) << std::endl;
             exit(malloc_err);
-        } else {
-            std::cout << "COLOR_MALLOC OK\n";
         }
     for (int i=0; i < V; i++){
         (*color)[i] = 0;
     }
 
-    //move graph to device memory
-    malloc_err = cudaMalloc(&graph_d, V * V * sizeof(bool));
-        if (malloc_err != cudaSuccess){
-            std::cout << "GRAPH_MALLOC cuda malloc ERROR happened: " << cudaGetErrorName(malloc_err) << std::endl;
-            exit(malloc_err);
-        } else {
-            std::cout << "GRAPH_MALLOC OK\n";
-        }
-    cudaMemcpy(graph_d, graph_h, V * V * sizeof(bool), cudaMemcpyHostToDevice);
-    
+    //find memory devision
+    int Nvertexes = min(V, floor((free / 2 - 16 * V)/(V+16))); // number of vertexes per one full-memory alocation
+
     //job for GPU (indexes of vertexes to process)
     int* job;
-    malloc_err = cudaMallocManaged(&job, V * sizeof(int));
+    malloc_err = cudaMallocManaged(&job, Nvertexes * sizeof(int));
         if (malloc_err != cudaSuccess){
             std::cout << "JOB_MALLOC cuda malloc ERROR happened: " << cudaGetErrorName(malloc_err) << std::endl;
             exit(malloc_err);
-        } else {
-            std::cout << "JOB_MALLOC OK\n";
         }
-    //fill job
-    for (int i=0; i < V; i++){
-        job[i] = i;
-/*debug*/// std::cout << "job " << i << " now is " << job[i] << "\n";
-    }
-    bool done = false;
-    
-    //start kernel
+    malloc_err = cudaMalloc(&graph_d, V * Nvertexes * sizeof(bool));
+        if (malloc_err != cudaSuccess){
+            std::cout << "GRAPH_MALLOC cuda malloc ERROR happened: " << cudaGetErrorName(malloc_err) << std::endl;
+            exit(malloc_err);
+        }
 
-    //repeat until find solition
-/*debug*/// int D = 0;
-    while (!done){
-        //sort job list and count amount of job
-        int N = 0;
-        for (int j=0; j < V; j++){
-            if (job[j] == -1){
-                for(int jj=j; jj < V; jj++){
-                    if (job[jj]!=-1){
-                        job[jj-1] = job[jj];
-                        job[jj] = -1;
+    //SEGMENT graph_d FROM HERE
+////////////////////////////////////////////
+    for (int V_start = 0; V_start < V; V_start += Nvertexes){
+        //actual amount of vertexes that awailable for proccessing
+        int Nv = min(Nvertexes, V - V_start);
+
+        //fill job
+        for (int i=V_start; i < Nv; i++){
+            job[i] = i;
+    /*debug*/// std::cout << "job " << i << " now is " << job[i] << "\n";
+        }
+        bool done = false;
+        
+        //start kernel
+
+        //move graph to device memory
+        cudaMemcpy(graph_d, graph_h + V_start * V, V * Nv * sizeof(bool), cudaMemcpyHostToDevice);
+    
+        //repeat until find solition
+    /*debug*/// int D = 0;
+        while (!done){
+            //sort job list and count amount of job
+            N = 0;
+            for (int j=0; j < Nv; j++){
+                if (job[j] == -1){
+                    for(int jj=j; jj < V; jj++){
+                        if (job[jj]!=-1){
+                            job[jj-1] = job[jj];
+                            job[jj] = -1;
+                        }
                     }
                 }
+                //cannot put `else`, as if job[j] == -1, the array will be different at this point
+                if (job[j] != -1) N++;
             }
-            //cannot put `else`, as if job[j] == -1, the array will be different at this point
-            if (job[j] != -1) N++;
-        }
-
-/*debug*/// for (int a=0; a<V; a++)
-/*debug*///     if (job[a]!=-1)
-/*debug*///         std::cout << "    job " << a << ": " << job[a] << " color: " << (*color)[job[a]] << "\n";
-/*debug*///     else std::cout << "    job " << a << ": " << job[a] << "\n";
-
-        //check colors nearby " << N << " vertexes
-
-        bool* near_colors;
-        cudaError malloc_err = cudaMallocManaged(&near_colors, V * N * sizeof(bool));
-            if (malloc_err != cudaSuccess){
-                std::cout << "NEAR_COLORS_1 cuda malloc ERROR happened: " << cudaGetErrorName(malloc_err) << std::endl;
-                exit(malloc_err);
+    
+    /*debug*/// for (int a=0; a<V; a++)
+    /*debug*///     if (job[a]!=-1)
+    /*debug*///         std::cout << "    job " << a << ": " << job[a] << " color: " << (*color)[job[a]] << "\n";
+    /*debug*///     else std::cout << "    job " << a << ": " << job[a] << "\n";
+    
+            //check colors nearby " << N << " vertexes
+    
+            bool* near_colors;
+            cudaError malloc_err = cudaMallocManaged(&near_colors, V * N * sizeof(bool));
+                if (malloc_err != cudaSuccess){
+                    std::cout << "NEAR_COLORS_1 cuda malloc ERROR happened: " << cudaGetErrorName(malloc_err) << std::endl;
+                    exit(malloc_err);
+                }
+    
+            for (int i=0; i < V*N; i++)
+                near_colors[i] = false;
+    
+            int nthreads = min(512, V*N);
+            int nblocks = ceil(float(V*N)/nthreads);
+            KernelNeighbourColor<<<nblocks, nthreads>>>(graph_d, *color, near_colors, V, job);
+            //sync CUDA and CPU
+            //cudaError synced = cudaDeviceSynchronize();
+            //    if (synced != cudaSuccess){
+            //        std::cout << "COLOR_NEARBY cuda sync ERROR happened: " << cudaGetErrorName(synced) << std::endl;
+            //        exit(synced);
+            //    }
+    
+    /*debug*/// for (int r=0; r < N; r++){
+    /*debug*///   printf("    near V %d: ",job[r]);
+    /*debug*///   for (int c=0; c < V; c++)
+    /*debug*///     printf(" %d",near_colors[r*V+c]);
+    /*debug*///   printf("\n");
+    /*debug*/// }        
+    
+            //find colors
+    
+            nthreads = min(512, N);
+            nblocks = ceil(float(N)/nthreads);
+            KernelSearchColor<<<nblocks, nthreads>>>(*color, near_colors, V, job);
+            //sync CUDA and CPU
+            synced = cudaDeviceSynchronize();
+                if (synced != cudaSuccess){
+                    std::cout << "FIND_COLOR cuda sync ERROR happened: " << cudaGetErrorName(synced) << std::endl;
+                    exit(synced);
+                }
+    
+            //cudaFree(near_colors);
+    
+    /*debug*/// for (int c=0; c<V; c++){
+    /*debug*///     printf("    V %d - color %d\n", c, (*color)[c]);
+    /*debug*/// }
+            
+            //check if need to work again (update `near_colors`)
+            //malloc_err = cudaMallocManaged(&near_colors, V * N * sizeof(bool));
+            //    if (malloc_err != cudaSuccess){
+            //        std::cout << "NEAR_COLORS_2 cuda malloc ERROR happened: " << cudaGetErrorName(malloc_err) << std::endl;
+            //        exit(malloc_err);
+            //    }
+    
+            for (int i=0; i < V*N; i++)
+                near_colors[i] = false;
+    
+            nthreads = min(512, V*N);
+            nblocks = ceil(float(V*N)/nthreads);
+            KernelNeighbourColor<<<nblocks, nthreads>>>(graph_d, *color, near_colors, V, job);
+            //sync CUDA and CPU
+            //synced = cudaDeviceSynchronize();
+            //    if (synced != cudaSuccess){
+            //        std::cout << "COLOR_NEARBY_CHECK cuda sync ERROR happened: " << cudaGetErrorName(synced) << std::endl;
+            //        exit(synced);
+            //    }
+    
+    /*debug*/// for (int r=0; r < N; r++){
+    /*debug*///   printf("    near V %d: ",job[r]);
+    /*debug*///   for (int c=0; c < V; c++)
+    /*debug*///     printf(" %d",near_colors[r*V+c]);
+    /*debug*///   printf("\n");
+    /*debug*/// }
+    
+            //update job
+            //int* new_job;
+            //cudaMallocManaged(&new_job, V * sizeof(bool));
+            //for (int j=0; j < V; j++){
+            //    new_job[j] = -1;
+            //}
+    
+            //nthreads = min(512, V*N);
+            //nblocks = ceil(float(V*N)/nthreads);
+            //KernelCheckColor<<<nblocks, nthreads>>>(graph_d, *color, V, job, new_job);
+            KernelCheckColor<<<nblocks, nthreads>>>(graph_d, *color, V, job, job);
+            //sync CUDA and CPU
+            synced = cudaDeviceSynchronize();
+                if (synced != cudaSuccess){
+                    std::cout << "JOB_UPDATE cuda sync ERROR happened: " << cudaGetErrorName(synced) << std::endl;
+                    exit(synced);
+                }
+            cudaFree(near_colors);
+    
+    /*debug*/// for (int a=0; a<V; a++)
+    /*debug*///     if (new_job[a]!=-1)
+    /*debug*///         std::cout << "    new_job " << a << ": " << new_job[a] << " color: " << (*color)[new_job[a]] << "\n";
+    /*debug*///     else std::cout << "    new_job " << a << ": " << new_job[a] << "\n";
+            
+            //swap job lists
+            //cudaFree(old_job);
+            //cudaFree(job);
+            //job = new_job;
+    
+            //check if done
+            done = true;
+            for(int i=0; i < V; i++){
+                if (job[i] != -1){
+    /*debug*///       printf("Need to work more\n");
+                    done = false;
+                    break;
+                }
             }
+    /*debug*/// D++;
+    /*debug*/// if (D == 4) done = true;
 
-        for (int i=0; i < V*N; i++)
-            near_colors[i] = false;
+        } //while not done
 
-        int nthreads = min(512, V*N);
-        int nblocks = ceil(float(V*N)/nthreads);
-        KernelNeighbourColor<<<nblocks, nthreads>>>(graph_d, *color, near_colors, V, job);
-        //sync CUDA and CPU
-        cudaError synced = cudaDeviceSynchronize();
-        if (synced != cudaSuccess){
-            std::cout << "COLOR_NEARBY cuda sync ERROR happened: " << cudaGetErrorName(synced) << std::endl;
-            exit(synced);
-        }
-
-/*debug*/// for (int r=0; r < N; r++){
-/*debug*///   printf("    near V %d: ",job[r]);
-/*debug*///   for (int c=0; c < V; c++)
-/*debug*///     printf(" %d",near_colors[r*V+c]);
-/*debug*///   printf("\n");
-/*debug*/// }        
-
-        //find colors
-
-        nthreads = min(512, N);
-        nblocks = ceil(float(N)/nthreads);
-        KernelSearchColor<<<nblocks, nthreads>>>(*color, near_colors, V, job);
-        //sync CUDA and CPU
-        synced = cudaDeviceSynchronize();
-        if (synced != cudaSuccess){
-            std::cout << "FIND_COLOR cuda sync ERROR happened: " << cudaGetErrorName(synced) << std::endl;
-            exit(synced);
-        }
-
-        cudaFree(near_colors);
-
-/*debug*/// for (int c=0; c<V; c++){
-/*debug*///     printf("    V %d - color %d\n", c, (*color)[c]);
-/*debug*/// }
-        
-        //check if need to work again (update `near_colors`)
-        malloc_err = cudaMallocManaged(&near_colors, V * N * sizeof(bool));
-            if (malloc_err != cudaSuccess){
-                std::cout << "NEAR_COLORS_2 cuda malloc ERROR happened: " << cudaGetErrorName(malloc_err) << std::endl;
-                exit(malloc_err);
-            }
-
-        for (int i=0; i < V*N; i++)
-            near_colors[i] = false;
-
-        nthreads = min(512, V*N);
-        nblocks = ceil(float(V*N)/nthreads);
-        KernelNeighbourColor<<<nblocks, nthreads>>>(graph_d, *color, near_colors, V, job);
-        //sync CUDA and CPU
-        synced = cudaDeviceSynchronize();
-        if (synced != cudaSuccess){
-            std::cout << "COLOR_NEARBY_CHECK cuda sync ERROR happened: " << cudaGetErrorName(synced) << std::endl;
-            exit(synced);
-        }
-
-/*debug*/// for (int r=0; r < N; r++){
-/*debug*///   printf("    near V %d: ",job[r]);
-/*debug*///   for (int c=0; c < V; c++)
-/*debug*///     printf(" %d",near_colors[r*V+c]);
-/*debug*///   printf("\n");
-/*debug*/// }
-
-        //update job
-        int* new_job;
-        cudaMallocManaged(&new_job, V * sizeof(bool));
-        for (int j=0; j < V; j++){
-            new_job[j] = -1;
-        }
-
-        nthreads = min(512, V*N);
-        nblocks = ceil(float(V*N)/nthreads);
-        KernelCheckColor<<<nblocks, nthreads>>>(graph_d, *color, V, job, new_job);
-        //sync CUDA and CPU
-        synced = cudaDeviceSynchronize();
-        if (synced != cudaSuccess){
-            std::cout << "JOB_UPDATE cuda sync ERROR happened: " << cudaGetErrorName(synced) << std::endl;
-            exit(synced);
-        }
-        cudaFree(near_colors);
-
-/*debug*/// for (int a=0; a<V; a++)
-/*debug*///     if (new_job[a]!=-1)
-/*debug*///         std::cout << "    new_job " << a << ": " << new_job[a] << " color: " << (*color)[new_job[a]] << "\n";
-/*debug*///     else std::cout << "    new_job " << a << ": " << new_job[a] << "\n";
-        
-        //swap job lists
-        //cudaFree(old_job);
-        cudaFree(job);
-        job = new_job;
-
-        //check if done
-        done = true;
-        for(int i=0; i < V; i++){
-            if (job[i] != -1){
-/*debug*///       printf("Need to work more\n");
-                done = false;
-                break;
-            }
-        }
-/*debug*/// D++;
-/*debug*/// if (D == 4) done = true;
-    }
+    } //for V_start
     cudaFree(job);
+    cudaFree(graph_d);
+
+//analyze result
+/////////////////////////////////////////////////
 
     //color counter
     int num_colors = 0;
